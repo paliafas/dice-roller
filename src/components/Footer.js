f176269969f6e8009192263a9b2d1ee5734335ee
paliafas <lpnexi@gmail.com>
2020-03-20 23:24:23 +0200

    Fix description. Remove font awesome

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,23 +5,14 @@ const Footer = () => {
         <footer id="main-footer">
             <div className="container">
                 <p>
-                    Made with{' '}
                     <a
-                        href="https://reactjs.org/"
+                        href="https://www.linkedin.com/in/pantelis-liafas-6438a8169/"
                         target="_blank"
                         rel="noopener noreferrer"
                     >
-                        <span className="react-word">React</span>
+                        <span className="signature-word">PL</span>
                     </a>
-                    .
                 </p>
-                <a
-                    href="https://www.linkedin.com/in/pantelis-liafas-6438a8169/"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <i className="fab fa-linkedin fa-2x"></i>
-                </a>
             </div>
         </footer>
     );
