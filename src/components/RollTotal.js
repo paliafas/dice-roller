0a4d99c5d77452285188298ac6af11965d047b92
paliafas <lpnexi@gmail.com>
2023-09-03 15:36:56 +0300

    Refactor RollTotal to use Tailwind

--- src/components/RollTotal.js
+++ src/components/RollTotal.js
@@ -2,9 +2,9 @@ import React from 'react';
 
 const RollTotal = ({ value }) => {
     return (
-        <section id="roll-total">
-            <div className="container">
-                <p className="roll-total-display">{value}</p>
+        <section className="h-[10%] bg-roll-total">
+            <div className="h-full flex justify-center items-center">
+                <p className="text-[4rem]">{value}</p>
             </div>
         </section>
     );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 module.exports = {
     content: ['./src/**/*.{js,jsx,ts,tsx}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'roll-total': '#111',
+                'roll-area': '#111',
+                controls: '#222',
+                'main-footer': '#4a437d',
+                d4: '#084c61',
+                d6: '#db504a',
+                d8: '#cf9038',
+                d10: '#4f6d7a',
+                d12: '#56a3a6',
+                d20: '#967aa1',
+                'button-control': '#333',
+            },
+        },
     },
     plugins: [],
 };
