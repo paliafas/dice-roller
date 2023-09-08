39a4594b4cf9e7442ea55b04a801fe43ee810a4a
paliafas <lpnexi@gmail.com>
2023-09-08 13:41:34 +0300

    Refactor Controls to Tailwind

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -4,9 +4,9 @@ import RollButton from './RollButton';
 import ClearButton from './ClearButton';
 
 const Controls = ({ addDie, rollDice, clearDice }) => (
-    <section id="controls">
-        <div className="container">
-            <div className="controls-content">
+    <section className="h-[20%] md:h-[10%] bg-controls">
+        <div className="h-full">
+            <div className="h-full grid grid-cols-[repeat(4,_1fr)] py-1 md:grid-cols-[repeat(8,_1fr)]">
                 <DieSelector die={{ sides: 4, roll: 4 }} addDie={addDie} />
                 <DieSelector die={{ sides: 6, roll: 6 }} addDie={addDie} />
                 <DieSelector die={{ sides: 8, roll: 8 }} addDie={addDie} />
