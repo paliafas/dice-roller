16282e1022a954f8a60f7192888ece7ae7d31411
paliafas <lpnexi@gmail.com>
2023-09-09 19:10:10 +0300

    Remove test utility from RollArea.js

--- src/components/RollArea.js
+++ src/components/RollArea.js
@@ -7,7 +7,7 @@ const RollArea = ({ dice, removeDie }) => {
     ));
 
     return (
-        <section className="bg-roll-area h-[65%] md:h-[75%] tall:bg-red px-0 py-8">
+        <section className="bg-roll-area h-[65%] md:h-[75%] px-0 py-8">
             <div className="h-[100%] container mx-auto px-4 max-w-[960px]">
                 <div className="h-[100%] grid grid-cols-[repeat(auto-fit,_minmax(20%,_0.25fr))] justify-center">
                     {diceElements}
